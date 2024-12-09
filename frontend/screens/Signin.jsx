28ce66fb195fed436776c2ab3e1cd6efcd972a23
Quentin <quentin.sebire@yahoo.com>
2024-12-09 12:35:34 +0100

    App.js Nav & Pages initialisation

---
+++ frontend/screens/Signin.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+function SigninScreen() {
+    return (
+        <View>
+            <Text>
+                SigninScreen
+            </Text>
+        </View>
+    )
+}
+
+export default SigninScreen
