6376241f5974c318ae32e33588237ac47637b459
Quentin <quentin.sebire@yahoo.com>
2024-12-09 16:56:13 +0100

    Button + Landing + App + Styleglobal

--- frontend/screens/Landing.jsx
+++ frontend/screens/Landing.jsx
@@ -1,28 +1,33 @@
 import { Text, View, SafeAreaView, StyleSheet } from "react-native"
-import ButtonRegular from "../components/buttons/Button-regular"
+import ButtonRegular from "../components/buttons/ButtonRegular"
 
-export default function LandingScreen({navigation}) {
+export default function LandingScreen({ navigation }) {
 
     const navigateToPatient = () => {
         return navigation.navigate('Signin')
     }
 
-    
+
 
     return (
         <SafeAreaView style={styles.container}>
             <View>
-            <Text>
-                Logo
-            </Text>
+                <Text>
+                    Logo
+                </Text>
             </View>
             <View>
-                {ButtonRegular('patient', navigateToPatient, 'buttonStroke')}
-            </View>            
-            <View>
-                {ButtonRegular('psy')}
+                <ButtonRegular text='patient' onPress={navigateToPatient} type='buttonStroke' />
             </View>
-
+            <View>
+                <ButtonRegular text='psy' onPress={navigateToPatient} type='buttonStroke' />
+            </View>
+            <View>
+                <ButtonRegular text='Test Psy' onPress={() => navigation.navigate("TherapistTabNavigator")} type='buttonStroke' />
+            </View>
+            <View>
+                <ButtonRegular text='Test patient' onPress={() => navigation.navigate("PatientTabNavigator")} type='buttonStroke' />
+            </View>
         </SafeAreaView>
     )
 }
