0fef34aead476d880c6359ebaa34de1fb6235275
Quentin <quentin.sebire@yahoo.com>
2024-12-10 10:38:30 +0100

    Input component

--- frontend/components/inputs/input.jsx
+++ frontend/components/inputs/input.jsx
@@ -1,18 +1,44 @@
-import React, {useState} from "react";
-import {TextInput, View, Text, StyleSheet} from 'react-native'
+import React, { useEffect, useState } from "react";
+import { TextInput, View, Text, StyleSheet } from 'react-native'
 
-export default function Input ({label, placeholder, errorMessage, value, onChangeText}) {
+export default function Input({
+    label,
+    placeholder,
+    errorMessage = "Merci de compléter ce champ.",
+    value,
+    onChangeText,
+    defaultValue = "",
+    inputMode = "text",
+    require = true
+}) {
 
     const [error, setError] = useState(false)
+    const [firstComplete, setFirstComplete] = useState(true)
 
-    if (value === "") {
-        setError(true)
+    useEffect(() => {
+        if (require && !firstComplete && value === "") {
+            setError(true)
+        }
+    }, [value])
+
+    const handleOnChangeText = (value) => {
+        if (firstComplete) {
+            setFirstComplete(false)
+        }
+        onChangeText(value)
     }
 
     return (
         <View>
             <Text style={styles.textTop} >{label}</Text>
-            <TextInput placeholder={placeholder} onChangeText={onChangeText} value={value} style={styles.input} />
+            <TextInput
+                placeholder={placeholder}
+                onChangeText={(value) => handleOnChangeText(value)}
+                value={value}
+                defaultValue={defaultValue}
+                inputMode={inputMode}
+                style={styles.input}
+            />
             {error && <Text style={styles.textBottom}>{errorMessage}</Text>}
         </View>
     )
@@ -23,15 +49,15 @@ const styles = StyleSheet.create({
         borderColor: "black",
         borderWidth: 1,
         borderRadius: 8,
-        width : 350,
-        height : 45,
-        fontSize : 18,
-        padding : 12,
+        width: 350,
+        height: 45,
+        fontSize: 18,
+        padding: 12,
     },
     textTop: {
-        fontSize : 14,
+        fontSize: 14,
     },
     textBottom: {
-        fontSize : 12,
+        fontSize: 12,
     }
 })
