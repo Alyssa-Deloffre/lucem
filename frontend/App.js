cd9cf6adee531ef99f47f2d8280b5fc9a586de56
Quentin <quentin.sebire@yahoo.com>
2024-12-13 11:13:23 +0100

    App.js - Menu

--- frontend/App.js
+++ frontend/App.js
@@ -55,48 +55,46 @@ const Tab = createBottomTabNavigator();
 // Patient tabs
 const PatientTabNavigator = () => {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          // Personnalisation de la tabBar
-          tabBarStyle: {
-            backgroundColor: "white",
-            borderTopLeftRadius: 16,
-            borderTopRightRadius: 16,
-            position: "absolute",
-          },
-          // Personnalisation des onglets
-          tabBarItemStyle: {
-            margin: 8,
-            padding: 8,
-            borderRadius: 8,
-            borderColor: "white",
-            borderWidth: 1,
-            height: 64,
-          },
-          tabBarActiveBackgroundColor: COLOR_GREEN[200],
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        // Personnalisation de la tabBar
+        tabBarStyle: {
+          backgroundColor: "white",
+          borderTopLeftRadius: 16,
+          borderTopRightRadius: 16,
+          position: "absolute",
+        },
+        // Personnalisation des onglets
+        tabBarItemStyle: {
+          margin: 8,
+          padding: 8,
+          borderRadius: 8,
+          borderColor: "white",
+          borderWidth: 1,
+          height: 64,
+        },
+        tabBarActiveBackgroundColor: COLOR_GREEN[200],
 
-          // Personnalisation des icones
-          tabBarIcon: ({ color, size }) => {
-            let iconName = "";
+        // Personnalisation des icones
+        tabBarIcon: ({ color, size }) => {
+          let iconName = "";
 
-            if (route.name === "Accueil") {
-              iconName = "home";
-            } else if (route.name === "Profil") {
-              iconName = "user";
-            }
+          if (route.name === "Accueil") {
+            iconName = "home";
+          } else if (route.name === "Profil") {
+            iconName = "user";
+          }
 
-            return <FontAwesome name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: COLOR_GREEN[1000],
-          tabBarInactiveTintColor: COLOR_PURPLE[1000],
-          headerShown: false,
-        })}
-      >
-        <Tab.Screen name="Accueil" component={PatientHomeScreen} />
-        <Tab.Screen name="Profil" component={PatientProfileScreen} />
-      </Tab.Navigator>
-    </SafeAreaView>
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: COLOR_GREEN[1000],
+        tabBarInactiveTintColor: COLOR_PURPLE[1000],
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Accueil" component={PatientHomeScreen} />
+      <Tab.Screen name="Profil" component={PatientProfileScreen} />
+    </Tab.Navigator>
   );
 };
 
